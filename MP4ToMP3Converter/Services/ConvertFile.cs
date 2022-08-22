0a63687898c5e2e39cc331b71ae04a047d58c167
mellow <stay.mellow.993@gmail.com>
2022-08-22 22:03:01 +0200

    Add Service for main task

---
+++ MP4ToMP3Converter/Services/ConvertFile.cs
@@ -0,0 +1,17 @@
+﻿using MP4ToMP3Converter.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MP4ToMP3Converter.Services
+{
+    internal class ConvertFile
+    {
+        private readonly Attributes _attributes;
+
+        public ConvertFile()
+        {
+            // set fields
+        }
+    }
+}

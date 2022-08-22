1f7f2a8de1a4e74f54311a490f84d7333b07eed4
mellow <stay.mellow.993@gmail.com>
2022-08-22 21:49:54 +0200

    Add Model to keep informations

---
+++ MP4ToMP3Converter/Model/Attributes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MP4ToMP3Converter.Model
+{
+    internal class Attributes
+    {
+        public string SourcePath { get; set; }
+        public string DestinationPath { get; set; }
+        public string Filename { get; set; }
+        public string FileExtension { get; set; }
+    }
+}

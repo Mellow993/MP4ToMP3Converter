5da0633687368b99bf4d07f2fbc8cf220567da95
mellow <stay.mellow.993@gmail.com>
2022-08-31 18:31:17 +0200

    Add save dialog

--- MP4ToMP3Converter/Services/OpenSelectDialog.cs
+++ MP4ToMP3Converter/Services/OpenSelectDialog.cs
@@ -6,20 +6,30 @@ namespace MP4ToMP3Converter.Services
     internal class OpenSelectDialog
     {
         private string _filepath = string.Empty;
-        public string OpenDialog()
+        private string _initialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+        public string SelectDialog()
         {
             using (System.Windows.Forms.OpenFileDialog openFileDialog = new System.Windows.Forms.OpenFileDialog())
             {
                 openFileDialog.Filter = "mp4 files (*.mp4)|*.mp4";
                 openFileDialog.Title = "Select your mp4 file";
-                openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
-                if(openFileDialog.ShowDialog() == DialogResult.OK)
+                openFileDialog.InitialDirectory = _initialDirectory;
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
                     _filepath = openFileDialog.FileName;
             }
             if (string.IsNullOrEmpty(_filepath))
                 return string.Empty;
             else
                 return _filepath;
+        }
+
+        public void SaveDialog()
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.InitialDirectory = _initialDirectory;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                _filepath = saveFileDialog.FileName;
         }
 
         //private string ShowDialog(System.Windows.Forms.OpenFileDialog openfiledialog)

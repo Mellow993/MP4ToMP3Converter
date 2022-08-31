871254c4d482c1cdd0513df001c3fdb37c8fc6d7
mellow <stay.mellow.993@gmail.com>
2022-08-31 16:31:47 +0200

    Reduce one method for return value

--- MP4ToMP3Converter/Services/OpenSelectDialog.cs
+++ MP4ToMP3Converter/Services/OpenSelectDialog.cs
@@ -5,22 +5,29 @@ namespace MP4ToMP3Converter.Services
 {
     internal class OpenSelectDialog
     {
-        private string _filepath;
-        public void OpenDialog()
+        private string _filepath = string.Empty;
+        public string OpenDialog()
         {
             using (System.Windows.Forms.OpenFileDialog openFileDialog = new System.Windows.Forms.OpenFileDialog())
             {
                 openFileDialog.Filter = "mp4 files (*.mp4)|*.mp4";
                 openFileDialog.Title = "Select your mp4 file";
                 openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
-                ShowDialog(openFileDialog);
+                if(openFileDialog.ShowDialog() == DialogResult.OK)
+                    _filepath = openFileDialog.FileName;
             }
+            if (string.IsNullOrEmpty(_filepath))
+                return string.Empty;
+            else
+                return _filepath;
         }
 
-        private void ShowDialog(System.Windows.Forms.OpenFileDialog openfiledialog)
-        {
-            if (openfiledialog.ShowDialog() == DialogResult.OK) ;
-                _filepath = openfiledialog.FileName;
-        }
+        //private string ShowDialog(System.Windows.Forms.OpenFileDialog openfiledialog)
+        //{
+        //    if (openfiledialog.ShowDialog() == DialogResult.OK)
+        //        _filepath = openfiledialog.FileName;
+        //    else
+        //        return _filepath = string.Empty;
+        //}
     }
 }
